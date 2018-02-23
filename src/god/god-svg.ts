04d2fd93938f3e6c9d3bab7a6b9027daf64ee438
Tony Medrano <tonymedrano@MacBook-Pro-de-Tony.local>
2018-02-23 12:24:37 +0100

    Testing first fucntionallity and classes, not big deal ;)

--- src/god/god-svg.ts
+++ src/god/god-svg.ts
@@ -3,7 +3,7 @@ export class SVG {
     private svg = document.createElementNS(this.xmlns, "svg");
 
     constructor() {
-        this.svg.setAttribute('style', 'border: 1px solid black');
+        this.svg.setAttribute('style', 'border: 1px dashed olive');
         this.svg.setAttribute('width', '600');
         this.svg.setAttribute('height', '250');
         this.svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
--- src/god/god.spec.ts
+++ src/god/god.spec.ts
@@ -1,8 +1,14 @@
 import { God } from "./god";
+import { SVG } from "./god-svg";
 
 describe('God initialized for the very first time', () => {
 
     it("should say 'God is running...'", () => {
         expect(new God().name).toEqual("God is running...");
+        console.log('God initialized for the very first time');
+    });
+
+    it("God should return a 'SVG.create()'", function () {
+        expect(SVG.create()).toEqual(jasmine.any(Object));
     });
 });
